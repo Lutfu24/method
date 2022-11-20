3f2192c17f10dac3b6adadd93df2e98426851449
Lutfu24 <lutfuhm@code.edu.az>
2022-11-20 16:33:12 +0400

    method

---
+++ method/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Common;
+
+namespace method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculate(81, 3, "/");
+        }
+        static void calculate(int number1, int number2, string operators = "+,-,*,/")
+        {
+            switch (operators)
+            {
+                case "+":
+
+                    Console.WriteLine(number1 + number2);
+                    break;
+                case "-":
+                    Console.WriteLine(number1 - number2);
+                    break;
+                case "*":
+                    Console.WriteLine(number1 * number2);
+                    break;
+                case "/":
+                    Console.WriteLine(number1 / number2);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
